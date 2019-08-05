447642b6c21e56efc6c4aa4332079a951478f3de
ph1p <phil@capshake.com>
2019-08-06 00:47:03 +0200

    feat(text-box): add ability to change alignment of text boxes
    Signed-off-by: ph1p <phil@capshake.com>

--- src/code.ts
+++ src/code.ts
@@ -2,6 +2,17 @@ figma.showUI(__html__, {
   visible: false
 });
 
+enum DirectionVertical {
+  TOP = 'TOP',
+  BOTTOM = 'BOTTOM',
+  CENTER = 'CENTER'
+}
+enum DirectionHorizontal {
+  LEFT = 'LEFT',
+  RIGHT = 'RIGHT',
+  CENTER = 'CENTER'
+}
+
 const solidColor = (r = 255, g = 0, b = 0) => ({
   type: 'SOLID',
   color: {
@@ -21,15 +32,19 @@ const getParentFrame = node => {
 
 async function main() {
   await figma.loadFontAsync({ family: 'Roboto', style: 'Regular' });
+  await figma.loadFontAsync({ family: 'Inter', style: 'Bold' });
 
   const createLine = async ({
     left = 0,
     top = 0,
     node,
     direction = 'horizontal',
-    name = 'Group'
+    name = 'Group',
+    verticalAlign = DirectionVertical.CENTER,
+    horizontalAlign = DirectionHorizontal.CENTER
   }) => {
-    const isRect = node.type === 'RECTANGLE';
+    const isRectOrEllipse =
+      node.type === 'RECTANGLE' || node.type === 'ELLIPSE';
 
     // needed elements
     const line = figma.createLine();
@@ -44,10 +59,17 @@ async function main() {
 
     const parentFrame = getParentFrame(node);
 
+    // margin for top and bottom
+    const directionMargin = 5;
+
+    // directions for text box
+    const txtVerticalAlign: DirectionVertical = verticalAlign;
+    const txtHorizontalAlign: DirectionHorizontal = horizontalAlign;
+
     let topNode = node.y;
     let leftNode = node.x;
 
-    if (isRect) {
+    if (isRectOrEllipse) {
       topNode += parentFrame.y;
       leftNode += parentFrame.x;
     }
@@ -55,29 +77,26 @@ async function main() {
     top += topNode;
     left += leftNode;
 
+    let lineOffset = 10;
+
     // LINE
     line.rotation = isHorizontal ? 0 : 90;
-    line.x =
-      left +
-      (!isHorizontal ? 1 : 0) +
-      (!isHorizontal ? (node.width / 100) * 10 : 0);
-    line.y = node.height + top - (isHorizontal ? (node.height / 100) * 10 : 0);
+
+    line.x = left + (!isHorizontal ? lineOffset : 0);
+    line.y = node.height + top - (isHorizontal ? lineOffset : 0);
 
     line.strokes = [].concat(solidColor());
 
     line.resize(node[heightOrWidth], 0);
-    line.strokeCap = 'ARROW_LINES';
+    line.strokeCap = 'ROUND';
 
     // LABEL
-    label.characters = node[heightOrWidth].toString();
-
-    if (isHorizontal) {
-      label.x = line.x + node.width / 2 - label.width / 2;
-      label.y = line.y - label.height - 10;
-    } else {
-      label.x = left + (node.width / 100) * 10 + label.width - 8;
-      label.y = node.height + top - node.height / 2 - label.height / 2;
-    }
+    label.characters = `${node[heightOrWidth]}`;
+    label.fontName = {
+      family: 'Inter',
+      style: 'Bold'
+    };
+    label.fontSize = 10;
 
     label.fills = [].concat(solidColor(255, 255, 255));
 
@@ -98,24 +117,98 @@ async function main() {
     const textGroup = figma.group([label, rect], group);
     textGroup.name = 'label';
 
+    // x, y for text box
+    const boxTop = top + paddingTopBottom / 2;
+    const boxLeft = left + paddingLeftRight / 2;
+
+    // place text group
+    if (isHorizontal) {
+      textGroup.x += boxLeft + node.width / 2 - textGroup.width / 2;
+      textGroup.y += boxTop + node.height - lineOffset - line.strokeWeight;
+
+      // vertical text align
+      if (txtVerticalAlign === DirectionVertical.CENTER) {
+        textGroup.y -= textGroup.height / 2;
+      } else if (txtVerticalAlign === DirectionVertical.BOTTOM) {
+        textGroup.y += directionMargin;
+      } else if (txtVerticalAlign === DirectionVertical.TOP) {
+        textGroup.y -= textGroup.height + directionMargin;
+      }
+
+      // horizontal text align
+      if (txtHorizontalAlign === DirectionHorizontal.CENTER) {
+        textGroup.x += 0;
+      } else if (txtHorizontalAlign === DirectionHorizontal.LEFT) {
+        textGroup.x -= node.width / 2 - textGroup.width / 2 - directionMargin;
+      } else if (txtHorizontalAlign === DirectionHorizontal.RIGHT) {
+        textGroup.x += node.width / 2 - textGroup.width / 2 - directionMargin;
+      }
+    } else {
+      textGroup.x += boxLeft + lineOffset;
+      textGroup.y += boxTop;
+
+      // vertical text align
+      if (txtVerticalAlign === DirectionVertical.CENTER) {
+        textGroup.y += node.height / 2 - textGroup.height / 2;
+      } else if (txtVerticalAlign === DirectionVertical.BOTTOM) {
+        textGroup.y += node.height - textGroup.height - directionMargin;
+      } else if (txtVerticalAlign === DirectionVertical.TOP) {
+        textGroup.y += directionMargin;
+      }
+
+      // vertical text align
+      if (txtHorizontalAlign === DirectionHorizontal.CENTER) {
+        textGroup.x -= textGroup.width / 2;
+      } else if (txtHorizontalAlign === DirectionHorizontal.LEFT) {
+        textGroup.x -= textGroup.width + directionMargin;
+      } else if (txtHorizontalAlign === DirectionHorizontal.RIGHT) {
+        textGroup.x += directionMargin;
+      }
+    }
+
+    // group.locked = true;
+
+    // line Positioning
+    // center horizontal
+    // if (isHorizontal) {
+    //   if (
+    //     txtVerticalAlign === DirectionVertical.BOTTOM ||
+    //     txtVerticalAlign === DirectionVertical.TOP
+    //   ) {
+    //     group.y -= directionMargin * 2;
+    //   }
+    //   group.y += lineOffset - (node.height - group.height) / 2;
+    // }
+
+    // center vertical
+    // if (!isHorizontal) {
+    //   console.log(node.width - group.width);
+    //   group.x -= lineOffset - node.width / 2;
+    // }
+
     return group;
   };
 
   for (const node of figma.currentPage.selection) {
-    const isRect = node.type === 'RECTANGLE';
+    const isRectOrEllipse =
+      node.type === 'RECTANGLE' || node.type === 'ELLIPSE';
     const isFrame = node.type === 'FRAME';
 
-    if (isFrame || isRect) {
+    if (isFrame || isRectOrEllipse) {
       const parentFrame = getParentFrame(node);
 
       const horizontalLine = await createLine({
         name: 'horizontal line',
+        verticalAlign: DirectionVertical.TOP,
+        horizontalAlign: DirectionHorizontal.CENTER,
         node
       });
 
       const verticalLine = await createLine({
         name: 'vertical line',
         direction: 'vertical',
+        verticalAlign: DirectionVertical.CENTER,
+        horizontalAlign: DirectionHorizontal.RIGHT,
         node
       });
 
@@ -131,6 +224,7 @@ async function main() {
   }
 }
 
+figma;
 main().then(() => {
   figma.closePlugin();
 });
