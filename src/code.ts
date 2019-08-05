b72a3191442bbf94d865a408bbdebbda27f29fbc
ph1p <phil@capshake.com>
2019-08-05 15:17:06 +0200

    refactor(methods): remove unnecessary code and move methods

--- src/code.ts
+++ src/code.ts
@@ -2,9 +2,25 @@ figma.showUI(__html__, {
   visible: false
 });
 
+const solidColor = (r = 255, g = 0, b = 0) => ({
+  type: 'SOLID',
+  color: {
+    r: r / 255,
+    g: g / 255,
+    b: b / 255
+  }
+});
+
+const getParentFrame = node => {
+  if (node.type === 'FRAME') {
+    return node;
+  } else {
+    return getParentFrame(node.parent);
+  }
+};
+
 async function main() {
   await figma.loadFontAsync({ family: 'Roboto', style: 'Regular' });
-  const nodes = [];
 
   const createLine = async ({
     left = 0,
@@ -13,6 +29,8 @@ async function main() {
     direction = 'horizontal',
     name = 'Group'
   }) => {
+    const isRect = node.type === 'RECTANGLE';
+
     // needed elements
     const line = figma.createLine();
     const rect = figma.createRectangle();
@@ -24,50 +42,44 @@ async function main() {
     const isHorizontal = direction === 'horizontal';
     const heightOrWidth = isHorizontal ? 'width' : 'height';
 
+    const parentFrame = getParentFrame(node);
+
+    let topNode = node.y;
+    let leftNode = node.x;
+
+    if (isRect) {
+      topNode += parentFrame.y;
+      leftNode += parentFrame.x;
+    }
+
+    top += topNode;
+    left += leftNode;
+
     // LINE
     line.rotation = isHorizontal ? 0 : 90;
     line.x =
       left +
       (!isHorizontal ? 1 : 0) +
       (!isHorizontal ? (node.width / 100) * 10 : 0);
-    line.y = top - (isHorizontal ? (node.height / 100) * 10 : 0);
+    line.y = node.height + top - (isHorizontal ? (node.height / 100) * 10 : 0);
 
-    line.strokes = [
-      {
-        type: 'SOLID',
-        color: {
-          r: 255 / 255,
-          g: 0 / 255,
-          b: 0 / 255
-        }
-      }
-    ];
+    line.strokes = [].concat(solidColor());
 
     line.resize(node[heightOrWidth], 0);
     line.strokeCap = 'ARROW_LINES';
-
 
     // LABEL
     label.characters = node[heightOrWidth].toString();
 
     if (isHorizontal) {
-      label.x = line.x + node[heightOrWidth] / 2 - label[heightOrWidth] / 2;
+      label.x = line.x + node.width / 2 - label.width / 2;
       label.y = line.y - label.height - 10;
     } else {
       label.x = left + (node.width / 100) * 10 + label.width - 8;
-      label.y = top - node.height / 2 - label.height / 2;
+      label.y = node.height + top - node.height / 2 - label.height / 2;
     }
 
-    label.fills = [
-      {
-        type: 'SOLID',
-        color: {
-          r: 255 / 255,
-          g: 255 / 255,
-          b: 255 / 255
-        }
-      }
-    ];
+    label.fills = [].concat(solidColor(255, 255, 255));
 
     // RECTANGLE
     rect.x = label.x - paddingLeftRight / 2;
@@ -77,16 +89,7 @@ async function main() {
       label.width + paddingLeftRight,
       label.height + paddingTopBottom
     );
-    rect.fills = [
-      {
-        type: 'SOLID',
-        color: {
-          r: 255 / 255,
-          g: 0 / 255,
-          b: 0 / 255
-        }
-      }
-    ];
+    rect.fills = [].concat(solidColor());
 
     // grouping
     const group = figma.group([line], node.parent);
@@ -98,38 +101,21 @@ async function main() {
     return group;
   };
 
-  const getParentFrame = node => {
-    if (node.type === 'FRAME') {
-      return node;
-    } else {
-      return getParentFrame(node.parent);
-    }
-  };
-
   for (const node of figma.currentPage.selection) {
-    if (node.type === 'FRAME' || node.type === 'RECTANGLE') {
+    const isRect = node.type === 'RECTANGLE';
+    const isFrame = node.type === 'FRAME';
+
+    if (isFrame || isRect) {
       const parentFrame = getParentFrame(node);
-
-      let top = node.height + node.y;
-      let left = node.x;
-
-      if (node.type === 'RECTANGLE') {
-        top += parentFrame.y;
-        left += parentFrame.x;
-      }
 
       const horizontalLine = await createLine({
         name: 'horizontal line',
-        left,
-        top,
         node
       });
 
       const verticalLine = await createLine({
         name: 'vertical line',
         direction: 'vertical',
-        left,
-        top,
         node
       });
 
